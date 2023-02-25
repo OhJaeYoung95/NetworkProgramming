17ff5f483dfb8f0c4ec141aca6991205ed90c294
OhJaeYoung <ostyoung@naver.com>
2023-02-25 16:53:17 +0900

    Listener Part2

--- ServerCore/Listener.cs
+++ ServerCore/Listener.cs
@@ -11,11 +11,13 @@ namespace ServerCore
     class Listener
     {
         Socket _listenSocket;
+        Action<Socket> _onAcceptHandler;
 
-        public void Init(IPEndPoint endPoint)
+        public void Init(IPEndPoint endPoint, Action<Socket> onAcceptHandler)
         {
             // 문지기
             _listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            _onAcceptHandler += onAcceptHandler;
 
             // 문지기 교육
             _listenSocket.Bind(endPoint);
@@ -26,10 +28,13 @@ namespace ServerCore
 
             SocketAsyncEventArgs args = new SocketAsyncEventArgs();
             args.Completed += new EventHandler<SocketAsyncEventArgs>(OnAcceptCompleted);
+            RegisterAccept(args);
         }
 
         void RegisterAccept(SocketAsyncEventArgs args)
         {
+            args.AcceptSocket = null;
+
             bool pending = _listenSocket.AcceptAsync(args);
             if (pending == false)
                 OnAcceptCompleted(null, args);
@@ -37,7 +42,15 @@ namespace ServerCore
 
         void OnAcceptCompleted(object sender, SocketAsyncEventArgs args)
         {
+            if(args.SocketError == SocketError.Success)
+            {
+                // TODO
+                _onAcceptHandler.Invoke(args.AcceptSocket);
+            }
+            else
+                Console.WriteLine(args.SocketError.ToString());
 
+            RegisterAccept(args);
         }
 
         public Socket Accept()
--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -12,6 +12,30 @@ namespace ServerCore
     class Program
     {
         static Listener _listener = new Listener();
+
+        static void OnAcceptHandler(Socket clientSocket)
+        {
+            try
+            {
+                // 받는다
+                byte[] recvBuff = new byte[1024];
+                int recvBytes = clientSocket.Receive(recvBuff);
+                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                Console.WriteLine($"[From Client] {recvData}");
+
+                // 보낸다
+                byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to MMORPG Server !");
+                clientSocket.Send(sendBuff);
+
+                // 쫓아낸다
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
         static void Main(string[] args)
         {
             // DNS (Domain Name System)
@@ -22,36 +46,13 @@ namespace ServerCore
 
             // www.rookies.com -> 123.123.123.12 / 도메인 이름을 사용하면 IP만 바꿀때 접근하기 용이함
 
+            _listener.Init(endPoint, OnAcceptHandler);
+            Console.WriteLine("Listening...");
 
-            try
+
+            while (true)
             {
-                _listener.Init(endPoint);
-
-                while (true)
-                {
-                    Console.WriteLine("Listening...");
-
-                    // 손님을 입장시킨다
-                    Socket clientSocket = _listener.Accept();
-
-                    // 받는다
-                    byte[] recvBuff = new byte[1024];
-                    int recvBytes = clientSocket.Receive(recvBuff);
-                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
-                    Console.WriteLine($"[From Client] {recvData}");
-
-                    // 보낸다
-                    byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to MMORPG Server !");
-                    clientSocket.Send(sendBuff);
-
-                    // 쫓아낸다
-                    clientSocket.Shutdown(SocketShutdown.Both);
-                    clientSocket.Close();
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
+                ;
             }
         }
     }
